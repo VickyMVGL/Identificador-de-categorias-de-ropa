37103a16ac72d4d996649582dac74b357bb002ee
VickyMVGL <mariaviclg2005@gmail.com>
2025-05-21 15:47:10 -0400

    Se realizo una seccion para tomar 25 datos y realizar las predicciones

--- ia.py
+++ ia.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 datos, metadatos = tfds.load('fashion_mnist', with_info=True, as_supervised=True)
 
@@ -80,6 +81,63 @@ datos_prueba = datos_prueba.batch(tam_lote)
 historial = modelo.fit(datos_entrenamiento, epochs=5, steps_per_epoch=math.ceil( num_ej_entrenamiento / tam_lote))
 
 # Evaluar el modelo
-
-plt.xlabel('Epochs')
+"""
+plt.xlabel('Epoca')
 plt.ylabel('Magnitud de perdida')
+plt.plot(historial.history['loss'])
+plt.show()
+"""
+
+for imagenes_prueba, etiquetas_prueba in datos_prueba.take(1):
+    imagenes_prueba = imagenes_prueba.numpy()
+    etiquetas_prueba = etiquetas_prueba.numpy()
+    predicciones = modelo.predict(imagenes_prueba)
+
+def graficar_imagen(i, predicciones_array, etiquetas_reales, imagenes):
+    predicciones_array, etiqueta_real, imagen = predicciones_array[i], etiquetas_reales[i], imagenes[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(imagen, cmap=plt.cm.binary)
+
+    etiqueta_prediccion = np.argmax(predicciones_array)
+    if etiqueta_prediccion == etiqueta_real:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(
+        nombres_clases[etiqueta_prediccion],
+        100*np.max(predicciones_array), 
+        nombres_clases[etiqueta_real], 
+        color=color
+    ))
+
+# Graficar imagenes
+
+def graficar_valor_arreglo(i, predicciones_array, etiquetas_reales):
+    predicciones_array, etiqueta_real = predicciones_array[i], etiquetas_reales[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+
+    grafica = plt.bar(range(10), predicciones_array, color='#777777')
+
+    plt.ylim([0, 1])
+
+    etiqueta_prediccion = np.argmax(predicciones_array)
+
+    grafica[etiqueta_prediccion].set_color('red')
+    grafica[etiqueta_real].set_color('blue')
+
+
+filas = 5
+columnas = 5
+num_imagenes = filas * columnas
+plt.figure(figsize=(2*2*columnas, 2*filas))
+for i in range(num_imagenes):
+    plt.subplot(filas, 2*columnas, 2*i+1)
+    graficar_imagen(i, predicciones, etiquetas_prueba, imagenes_prueba)
+    plt.subplot(filas, 2*columnas, 2*i+2)
+    graficar_valor_arreglo(i, predicciones, etiquetas_prueba)
+    
