26dd8cbbaece7f718edb7f83408d992738223836
VickyMVGL <mariaviclg2005@gmail.com>
2025-05-22 10:18:51 -0400

    Se hizo un pequeño ajuste para entrenar correctamente el modelo

--- Identificador de numero/numeros.py
+++ Identificador de numero/numeros.py
@@ -6,7 +6,7 @@ import math
 
 # descargar el dataset de mnist (Numeros escritos a mano)
 
-datos, metadados = tfds.load ("mnist", as_supervised=True, with_info=True)
+datos, metadatos = tfds.load ("mnist", as_supervised=True, with_info=True)
 
 # Obtener en variables separadas los datos de entrenamiento (60k) y de prueba (10k)
 
@@ -21,9 +21,16 @@ def normalizar(imagen, etiqueta):
 datos_entrenamiento = datos_entrenamiento.map(normalizar)
 datos_prueba = datos_prueba.map(normalizar)
 
-plt.figure(figsize=(10, 10))
+# Agregar datos a cache
+datos_entrenamiento = datos_entrenamiento.cache()
+datos_prueba = datos_prueba.cache()
+
+
+#plt.figure(figsize=(10, 10))
 
 # Definir las clases (etiquetas) para los dígitos 0-9
+
+"""
 clases = [str(i) for i in range(10)]
 
 for i, (imagen, etiqueta) in enumerate(datos_entrenamiento.take(25)):
@@ -35,7 +42,7 @@ for i, (imagen, etiqueta) in enumerate(datos_entrenamiento.take(25)):
     plt.imshow(imagen, cmap=plt.cm.binary)
     plt.xlabel(clases[int(etiqueta)])
 plt.show()
-
+"""
 
 #Creando el modelo de red neuronal
 
@@ -48,8 +55,21 @@ modelo = tf.keras.models.Sequential([
 ])
 
 # Realizando el entrenamiento del modelo
-historial = modelo.fit(
-    datos_entrenamiento,
-    epochs=60,
-    steps_per_epoch=math.ceil(60000 / 32),
-)
+modelo.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+num_ej_entrenamiento = metadatos.splits['train'].num_examples
+num_ej_prueba = metadatos.splits['test'].num_examples
+
+tam_lote = 32
+
+datos_entrenamiento = datos_entrenamiento.repeat().shuffle(num_ej_entrenamiento).batch(tam_lote)
+datos_prueba = datos_prueba.batch(tam_lote)
+
+historial = modelo.fit(datos_entrenamiento, epochs=5, steps_per_epoch=math.ceil( num_ej_entrenamiento / tam_lote))
+
+
+# Exportar el modelo a un explorador
+
+modelo.save("modelo_mnist.h5")
